446b7dc8b9343bcbe23833c393c3a8a872642ea2
Advaith B G <hereliesadvaith@gmail.com>
2024-01-13 22:34:37 +0530

    [ADD] completions and snippets

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,29 +3,35 @@ return {
         "williamboman/mason.nvim",
         config = function()
             require("mason").setup()
-        end
+        end,
     },
     {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
                 ensure_installed = {
-                    "lua_ls", "pyright",
-                }
+                    "lua_ls",
+                    "pyright",
+                },
             })
-        end
+        end,
     },
     {
         "neovim/nvim-lspconfig",
         config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local lspconfig = require("lspconfig")
             -- Lua
-            lspconfig.lua_ls.setup({})
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
             -- Python
-            lspconfig.pyright.setup({})
+            lspconfig.pyright.setup({
+                capabilities = capabilities,
+            })
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
             vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-        end
+        end,
     },
 }
